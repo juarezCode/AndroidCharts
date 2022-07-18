aaf2f4399426c64e9532d46ee66428da6316a36d
Juarez Roberto <juarez.unknown@gmail.com>
2022-07-18 14:54:41 -0500

    :sparkles: add pie charte example

--- app/src/main/java/com/example/androidcharts/MainActivity.kt
+++ app/src/main/java/com/example/androidcharts/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.androidcharts
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 
@@ -20,16 +20,29 @@ class MainActivity : AppCompatActivity() {
 
         val chart = findViewById<View>(R.id.barchart) as BarChart
         chart.apply {
-            this.isEnabled = false
-            this.setMaxVisibleValueCount(60)
+            this.setTouchEnabled(false)
             this.setPinchZoom(false)
             this.setDrawBarShadow(false)
             this.setDrawGridBackground(false)
             this.description = null
+            this.axisLeft.isEnabled = true
+            this.axisLeft.setDrawAxisLine(false)
+            this.axisRight.isEnabled = false
+            this.legend.isEnabled = false
         }
 
-        val xAxis = chart.xAxis
-        xAxis.apply {
+        val pieChart = findViewById<View>(R.id.pieChart) as PieChart
+        pieChart.apply {
+            setTouchEnabled(false)
+            setUsePercentValues(true)
+            this.description = null
+            setExtraOffsets(5f, 10f, 5f, 5f)
+            dragDecelerationFrictionCoef = 0.95f
+            isDrawHoleEnabled = false
+            this.legend.isEnabled = false
+        }
+
+        chart.xAxis.apply {
             position = XAxis.XAxisPosition.BOTTOM
             setDrawAxisLine(false)
             setDrawGridLines(false)
@@ -37,10 +50,6 @@ class MainActivity : AppCompatActivity() {
             labelCount = 4 // dinamic
         }
 
-        chart.axisLeft.isEnabled = true
-        chart.axisLeft.setDrawAxisLine(false)
-        chart.axisRight.isEnabled = false
-        chart.legend.isEnabled = false
 
         val entries = mutableListOf<BarEntry>()
         entries.add(BarEntry(1f, 1f))
@@ -62,9 +71,39 @@ class MainActivity : AppCompatActivity() {
 
         val data = BarData(dataSets)
         data.barWidth = 0.5f
+        data.isHighlightEnabled = false
         chart.data = data
         chart.setFitBars(true)
         chart.invalidate()
+
+        val pieEntries = mutableListOf<PieEntry>()
+        pieEntries.add(PieEntry(1f))
+        pieEntries.add(PieEntry(1f))
+        pieEntries.add(PieEntry(2f))
+        pieEntries.add(PieEntry(1f))
+
+        val pieDataSet = PieDataSet(pieEntries, "Pie Chart")
+        pieDataSet.sliceSpace = 0f
+        pieDataSet.selectionShift = 20f
+        pieDataSet.colors = mutableListOf(
+            ContextCompat.getColor(this, android.R.color.holo_blue_light),
+            ContextCompat.getColor(this, android.R.color.holo_green_light),
+            ContextCompat.getColor(this, android.R.color.holo_orange_light),
+            ContextCompat.getColor(this, android.R.color.holo_red_light)
+        )
+        pieDataSet.valueLinePart1OffsetPercentage = 100f
+        pieDataSet.valueLinePart1Length = 0.7f
+        pieDataSet.valueLinePart2Length = 0.3f
+        pieDataSet.yValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
+
+        val pieData = PieData(pieDataSet)
+        pieData.setValueFormatter(MyFormatter())
+        pieData.setValueTextSize(11f)
+        pieData.setValueTextColor(Color.BLACK)
+
+        pieChart.data = pieData
+        pieChart.highlightValues(null)
+        pieChart.invalidate()
 
     }
 }
